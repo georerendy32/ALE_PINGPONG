71b48b5c775f2d74ee50bc1077a241422edf7823
georerendy32 <georerendy32@guru.sma.belajar.id>
2024-10-21 20:09:23 +0700

    update baru

--- m6p3.py
+++ m6p3.py
@@ -34,7 +34,7 @@ class Player(GameSprite):
 #game scene:
 back = (200, 255, 255) #background color (background)
 win_width = 600
-win_height = 500
+win_height = 600
 window = display.set_mode((win_width, win_height))
 window.fill(back)
 
